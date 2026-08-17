105c95286ba744ad7d6aa4569ec7ebc74595a58b
johnny <dark_skateboarding2@hotmail.com>
2026-08-17 19:15:19 +0300

    feat(utils): add db connection to mongodb

---
+++ src/utils/db.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+
+const MONGO_URI = process.env.MONGO_URI || '';
+
+export const connectDB = async() => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("MongoDB connected");
+    } catch (err) {
+        console.log("MongoDB connection error:", err);
+        process.exit(1);
+    }
+}
