fa3caadf2eb6fdc6dcafd8fc4cd7d18d03144f9e
johnny <dark_skateboarding2@hotmail.com>
2026-08-31 19:11:34 +0300

    chore(server): configure custom DNS servers for mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
-import app from "./app";
-import { connectDB } from "./utils/db";
+import dotenv from 'dotenv';
+import app from './app';
+import { connectDB } from './utils/db';
 
-const start = async() => {
+dotenv.config();
+
+import dns from 'node:dns'; 
+
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+const start = async () => {
     await connectDB();
 
-app.listen(3001, () => {
-    console.log('Server is up');
-})
-}
+    app.listen(3001, () => {
+        console.log('Server is up');
+    });
+};
 
 start();
 
